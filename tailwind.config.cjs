d095b5f0a5845508251b3fdb7a4eed6c129e86bd
Antonio Obradović <antonio.obra8@gmail.com>
2023-03-06 22:04:06 +0100

    created header section and about section, navbar wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Work Sans", "sans-serif"],
+        strech: ["Strech Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
